9e38f572a3ed67851679e209be7f056a0d42fc64
Kenny Mui <klmui@wisc.edu>
2019-11-30 00:17:45 -0600

    Removed index.html and moved it to home.ejs

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
-// app.get("/", function(req, res) {
-//   res.render("home");
-// });
+app.get("/", function(req, res) {
+  res.render("home");
+});
 
 app.get("/CreateEvent", function(req, res) {
   res.render("CreateEvent");
