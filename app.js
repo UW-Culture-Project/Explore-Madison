2082a61133c80d0b142d1f34ceb3c1c60e3b05ab
Kenny Mui <klmui@wisc.edu>
2019-11-30 00:01:23 -0600

    Added new calendar

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var bodyParser = require("body-parser");
 // var request = require("request");
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-  res.render("home");
-});
+// app.get("/", function(req, res) {
+//   res.render("home");
+// });
 
 app.get("/CreateEvent", function(req, res) {
   res.render("CreateEvent");
