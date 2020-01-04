1f3d5a98b1fc2b9d904ed3c8c23971ee112cba75
Kenny Mui <klmui@wisc.edu>
2020-01-04 02:34:00 -0600

    create user schema (user.js)

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose              = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+// Schema setup
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+// Adds passportLocalMongoose methods to UserSchema
+UserSchema.plugin(passportLocalMongoose);
+
+// module.exports allows us to use this schema in app.js
+module.exports = mongoose.model("User", UserSchema);
