571e279cbc83668e2d0a30af8082cd99ea4d7d10
Khoa Thien Le (Harry) <ktle4@wisc.edu>
2020-01-13 02:52:07 -0500

    add upvote and downvote routes

--- routes/events.js
+++ routes/events.js
@@ -20,12 +20,12 @@ router.get('/', function(req, res) {
   });
 });
 
-// SHOW - create event page only if the user is logged in
+// NEW ROUTE - Show create event form only if the user is logged in
 router.get('/new', isLoggedIn, function(req, res) {
   res.render('events/new');
 });
 
-// CREATE ROUTE - only create a new event if the user is logged in
+// CREATE ROUTE - Only create a new event if the user is logged in
 router.post('/', isLoggedIn, function(req, res) {
   // Sanitize the event so no Script tags can be run
   req.body.event.body = req.sanitize(req.body.event.body);
@@ -58,7 +58,7 @@ router.post('/', isLoggedIn, function(req, res) {
   });
 });
 
-// SHOW individual event
+// SHOW ROUTE - Show individual event
 router.get('/:id', function(req, res) {
   Event.findById(req.params.id, function(err, event) {
     if (err) {
@@ -67,6 +67,72 @@ router.get('/:id', function(req, res) {
       res.render('events/show', {
         event: event
       });
+    }
+  });
+});
+
+// UPVOTE ROUTE
+router.put('/:id/upvote', isLoggedIn, function(req, res) {
+  var updatedInfo = {};
+  Event.findById(req.params.id, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (event.upvoted) {
+        // Already upvoted, now undo it.
+        updatedInfo.upvoted = false;
+        updatedInfo.votes -= 1;
+      } else if (event.downvoted) {
+        // Already downvoted, now flip to upvote, add 2 votes.
+        updatedInfo.upvoted = true;
+        updatedInfo.downvoted = false;
+        updatedInfo.votes += 2;
+      } else {
+        // Neither upvoted or downvoted, now upvote, add 1 vote.
+        updatedInfo.upvoted = true;
+        updatedInfo.votes += 1;
+      }
+    }
+  });
+
+  Event.findByIdAndUpdate(req.params.id, updatedInfo, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/events/' + req.params.id);
+    }
+  });
+});
+
+// DOWNVOTE ROUTE
+router.put('/:id/downvote', isLoggedIn, function(req, res) {
+  var updatedInfo = {};
+  Event.findById(req.params.id, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (event.downvoted) {
+        // Already downvoted, now undo it.
+        updatedInfo.downvoted = false;
+        updatedInfo.votes += 1;
+      } else if (event.downvoted) {
+        // Already upvoted, now flip to downvote, subtract 2 votes.
+        updatedInfo.downvoted = true;
+        updatedInfo.upvoted = false;
+        updatedInfo.votes -= 2;
+      } else {
+        // Neither upvoted or downvoted, now downvote, subtract 1 vote.
+        updatedInfo.downvoted = true;
+        updatedInfo.votes -= 1;
+      }
+    }
+  });
+
+  Event.findByIdAndUpdate(req.params.id, updatedInfo, function(err, event) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/events/' + req.params.id);
     }
   });
 });
